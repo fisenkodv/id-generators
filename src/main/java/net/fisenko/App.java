b5e405f4ae3e477e22c650a6eba50848ef43516e
Dmitry Fisenko <fisenkodv@gmail.com>
2020-06-20 22:09:20 -0400

    chore: add Twitter Snowflake, add extra files

---
+++ src/main/java/net/fisenko/App.java
@@ -0,0 +1,46 @@
+package net.fisenko;
+
+import net.fisenko.utils.ids.snowflake.IdGenerator;
+import net.fisenko.utils.ids.snowflake.InvalidSystemClockException;
+import net.fisenko.utils.ids.snowflake.MaskConfig;
+import net.fisenko.utils.ids.snowflake.SequenceOverflowException;
+import net.fisenko.utils.ids.snowflake.SnowflakeIdGenerator;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class App {
+    public static void main(String[] args) throws InvalidSystemClockException, SequenceOverflowException {
+        // June 1st 2020 is the epoch
+        OffsetDateTime epoch = OffsetDateTime.of(LocalDateTime.of(2020, 6, 1, 0, 0, 0), ZoneOffset.UTC);
+        // Create a mask configuration of 45 bits for timestamp, 2 for generator-id and 16 for sequence
+        MaskConfig maskConfig = new MaskConfig((byte) 45, (byte) 2, (byte) 16);
+
+        IdGenerator<Long> generator = new SnowflakeIdGenerator(0, epoch, maskConfig);
+
+        // Let's ask the mask configuration how many generators we could instantiate in this setup (2 bits)
+        System.out.printf("Max. generators: %d", maskConfig.getMaxGenerators());
+        System.out.println();
+
+        // Let's ask the mask configuration how many sequential Id's we could generate
+        // in a single ms in this setup (16 bits)
+        System.out.printf("Id's/ms per generator: %d", maskConfig.getMaxSequenceIds());
+        System.out.println();
+
+        // Let's calculate the number of Id's we could generate, per ms, should we use
+        // the maximum number of generators
+        System.out.printf("Id's/ms total: %d", maskConfig.getMaxGenerators() * maskConfig.getMaxSequenceIds());
+        System.out.println();
+
+        // Let's ask the mask configuration for how long we could generate Id's before
+        // we experience a 'wraparound' of the timestamp
+        System.out.printf("Wraparound interval: %s", maskConfig.getWrapAroundInterval(generator.getTimeSource()));
+        System.out.println();
+
+        // And finally: let's ask the mask configuration when this wraparound will happen
+        // (we'll have to tell it the generator's epoch)
+        System.out.printf("Wraparound date: %s", maskConfig.getWrapAroundDate(generator.getEpoch(), generator.getTimeSource()));
+        System.out.println();
+    }
+}
